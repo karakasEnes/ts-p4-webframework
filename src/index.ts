7945de8e7ebbfb9e8077a1feba5402a109463198
Enes Karakaş <enes.karakas.e@gamil.com>
2022-02-07 14:14:39 +0300

    User class, and events propert importance covered

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('hey there');
+import { User } from './models/User';
+
+const user = new User({ name: 'stephen', age: 30 });
+
+user.set({ name: 'enes' });
+
+user.get('name');
+
+user.set({ name: 'luna', age: 21 });
+
+user.get('name');
+user.get('age');
